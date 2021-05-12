979ae4c4e8f5b276b52b0c2d61e78a6041d5fe21
canto1380 <atapenalba16@gmail.com>
2021-05-11 22:19:12 -0300

    Se agregaron validaciones al login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,48 +1,45 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Image, Form, Button } from "react-bootstrap";
 
-import { Link, withRouter }   from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import Swal from "sweetalert2";
+import { campoRequerido, emailValidacion } from "../helpers/helpers";
 
 import ImgPortada from "../img/Inicio-registro.jpg";
 import MsjError from "./MsjError";
 
 const Login = (props) => {
-  const {user} = props
+  const { user } = props;
   /* State */
   const [err, setErr] = useState(false); // Bandera
   const [usuario, setUsuario] = useState({
-    email:'',
-    password:''
-  })
+    email: "",
+    password: "",
+  });
   const validacion = {
     id: "",
-    token: ""
+    token: "",
   };
 
   /*variables */
-  let mensaje
+  let mensaje;
 
-  const handleValores =(e) =>{
+  const handleValores = (e) => {
     setUsuario({ ...usuario, [e.target.name]: e.target.value });
-  }
+  };
 
-  const handleSubmit =(e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
     user.map((u) => {
-      if(usuario.email !== u.email){
-        setErr(true);
-        setTimeout(() => {
+      if (campoRequerido(usuario.email) && emailValidacion(usuario.email)) {
+        setErr(false);
+        if (u.email === usuario.email && u.clave === usuario.password) {
+          /* Local Storage */
           setErr(false);
-        }, 3000);
-      } else{
-        if(u.email === usuario.email && u.clave === usuario.password){
-            /* Local Storage */
-            setErr(false)
-            validacion.id = u.id;
-            validacion.token ='dsafdgre32rfgdhh5rgvfdg435345'
-            localStorage.setItem('jwt',JSON.stringify(validacion))
-           
+          validacion.id = u.id;
+          validacion.token = "dsafdgre32rfgdhh5rgvfdg435345";
+          localStorage.setItem("jwt", JSON.stringify(validacion));
+
           /*Swal */
           let timerInterval;
           Swal.fire({
@@ -71,19 +68,24 @@ const Login = (props) => {
               props.history.push("/");
             }
           });
-        } else{
+        } else {
           setErr(true);
           setTimeout(() => {
             setErr(false);
           }, 3000);
         }
+      } else {
+        setErr(true);
+          setTimeout(() => {
+            setErr(false);
+          }, 3000);
       }
     });
-  }
-  if(err){
-    mensaje =(
-      <MsjError text1="Datos incorrectos" text2 ="Intentelo nuevamente."/>
-    )
+  };
+  if (err) {
+    mensaje = (
+      <MsjError text1="Datos incorrectos" text2="Intentelo nuevamente." />
+    );
   }
 
   return (
@@ -94,42 +96,54 @@ const Login = (props) => {
       <Row className="mt-3 d-flex justify-content-around">
         <Col sm={6} md={8}>
           <div className="">
-            {/* <h1 className="text-center display-1 mb-3">Bienvenido!</h1> */}
             <Image className="w-100" src={ImgPortada} rounded />
           </div>
         </Col>
         <Col sm={6} md={4}>
           <div className="">
-            
-              <h1 className="text-center">Ingrese a su cuenta</h1>
-              <div>
-                <Form className="my-4" onSubmit={handleSubmit}>
-                  <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" name="email" placeholder="nombre@gmail.com" onChange={handleValores}/>
-                  </Form.Group>
+            <h1 className="text-center">Ingrese a su cuenta</h1>
+            <div>
+              <Form className="my-4" onSubmit={handleSubmit}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    placeholder="nombre@gmail.com"
+                    onChange={handleValores}
+                  />
+                </Form.Group>
 
-                  <Form.Group className="my-4" controlId="formBasicPassword">
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control type="password" name="password" onChange={handleValores}/>
-                  </Form.Group>
-                  <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Recordarme" defaultChecked/>
-                  </Form.Group>
-                  <Button variant="primary" className="my-2 w-100" type="submit">
-                    Ingresar
-                  </Button>
-                  <div>{mensaje}</div>
-                  <div className="text-center">
-                    <Link to={'/'}>¿Olvidó su clave?</Link>
-                  </div>
-                </Form>
-                <hr/>
+                <Form.Group className="my-4" controlId="formBasicPassword">
+                  <Form.Label>Contraseña</Form.Label>
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    onChange={handleValores}
+                  />
+                </Form.Group>
+                <Form.Group controlId="formBasicCheckbox">
+                  <Form.Check
+                    type="checkbox"
+                    label="Recordarme"
+                    defaultChecked
+                  />
+                </Form.Group>
+                <Button variant="primary" className="my-2 w-100" type="submit">
+                  Ingresar
+                </Button>
+                <div>{mensaje}</div>
                 <div className="text-center">
-                  <p>¿No tienes cuenta? <Link to={'/'}>Registrate!</Link></p>
+                  <Link to={"/"}>¿Olvidó su clave?</Link>
                 </div>
+              </Form>
+              <hr />
+              <div className="text-center">
+                <p>
+                  ¿No tienes cuenta? <Link to={"/"}>Registrate!</Link>
+                </p>
               </div>
-            
+            </div>
           </div>
         </Col>
       </Row>
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,6 +1,24 @@
+/* Si el admin esta logueado */
 export const isAuthenticated =() =>{
     if(localStorage.getItem('jwt')){
         return localStorage.getItem('jwt')
+    } else {
+        return false
+    }
+}
+
+export const campoRequerido = (value) =>{
+    if(value.trim() === ''){
+        return false
+    } else {
+        return true
+    }
+}
+
+export const emailValidacion = (value) =>{
+    let expresion = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    if(value.trim() === expresion){
+        return true
     } else {
         return false
     }
