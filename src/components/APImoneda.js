1bcaec599000c94a037e460f39eab8c5d2b88751
Alejandro Poviña <alejandro.povina@gmail.com>
2021-05-19 02:25:22 -0300

    se ajusto detalles de alineacion en APImonedas

--- src/components/APImoneda.js
+++ src/components/APImoneda.js
@@ -11,8 +11,6 @@ const APImoneda = () => {
          consultarAPImoneda();
      },[]);
 
-    //  const of = await fetch("https://api.estadisticasbcra.com/usd_of")
-
      const consultarAPImoneda = async()=>{
          try{
             
@@ -38,20 +36,17 @@ const APImoneda = () => {
         
       <div className="boxAPI d-flex justify-content-evenly align-items-around small">
          
-              <div>
+              
               <p><b>Dolar Oficial</b></p>
-              </div>
-              <div>
+              
               <p>compra:  ${oficial.compra}</p>
         <p>venta: ${oficial.venta}</p>
-              </div>
-              <div >
+              
               <p><b>Dolar Blue</b></p>
-              </div>
-              <div >
+              
               <p>Dolar Blue compra: ${blue.compra}</p>
           <p>Dolar Blue venta: ${blue.venta}</p>
-              </div>
+              
 
           </div>
              
