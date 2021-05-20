7d140c296a63db1370a88911c96430a011aab745
unknown <nguardoruiz@gmail.com>
2021-05-20 13:50:03 -0300

    Se comienza maquetado principal del error 404

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,21 +1,23 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ImgErr404 from "../img/error-404-pagina-no-encontrada-hombre-barriendo-piso_82574-10307.jpg";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { Container } from "react-bootstrap";
+import { Image } from "react-bootstrap";
 
 const Error404 = () => {
   return (
-    <Container>
-      <h1 className="text-center">Error 404 en proceso</h1>
-      <p className="text-center">Imagen a Montar</p>
+    <div>
       <div className="d-flex justify-content-evenly">
-        <Link className="btn btn-primary " to={"/"}>
+        <Link className="btn btn-primary w-100 " to={"/"}>
           <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>
         </Link>
       </div>
-    </Container>
+      <div>
+        <Image className="w-100" src={ImgErr404} rounded />
+      </div>
+    </div>
   );
 };
 
