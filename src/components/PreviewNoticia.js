c63c4dfb9eed8b2f8f10e25f80b2040aa2888960
Alejandro Poviña <alejandro.povina@gmail.com>
2021-06-09 02:12:10 -0300

    se modifico logico de mostrar categoria en  PreviewNoticia

--- src/components/PreviewNoticia.js
+++ src/components/PreviewNoticia.js
@@ -5,27 +5,7 @@ import { Link, useParams } from "react-router-dom";
 const PreviewNoticia = (props) => {
   const { id } = useParams();
   const {tok,noticias} = props
-  // states
-  const [noticia, setNoticia] = useState({});
-
-  useEffect(() => {
-    consultarNoticia();
-  }, []);
-
-console.log(noticias)
-  const consultarNoticia = async () => {
-    try {
-      const url = process.env.REACT_APP_API_URL + "/noticias/" + id;
-      const respuesta = await fetch(url);
-      if (respuesta.status === 200) {
-        const resp = await respuesta.json();
-        setNoticia(resp);
-      }
-    } catch (error) {
-      console.log(error);
-      //cartel de error
-    }
-  };
+  
 
   return (
     <Container>
@@ -36,52 +16,58 @@ console.log(noticias)
           publicarla.
         </p>
       </div>
-      <Card className="mt-5 mb-3">
-        <Card.Header>
-          <p className="display-6">
-            {noticia.categoria}
-          </p>
-        </Card.Header>
-        <Card.Body>
-          <Card.Title>
-            <h1>
-              {noticia.titulo}
-            </h1>
-          </Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            <h3>
-              {noticia.descripcion}
-            </h3>
-          </Card.Subtitle>
-          <div className="row">
-            <div className="col-sm-12 col-md-8">
-              <p>
-                {noticia.descripNoticia}
-              </p>
-            </div>
-            <div className="col-sm-12 col-md-4">
-            <div className="row">
-              <div className="col-12 mb-2">
-                <Figure>
-                  <Image
-                    className="img-fluid "
-                    src={noticia.foto}
-                  />
-                </Figure>
-              </div>
-              <div className="col-12 border mb-2">
-                <h5>
-                  {noticia.pieDeFoto}
-                </h5>
-              </div>
-            </div>
-            </div>
-          </div>
-        </Card.Body>
-        <Card.Footer className="text-muted">
-          <h5>{noticia.autor}</h5>
-        </Card.Footer>
+          {noticias.map((not)=>{
+            if(not._id === id){
+              return (
+                <Card key={not._id} className="mt-5 mb-3">
+                <Card.Header>
+                   <p className="display-6">
+                     {not.categoria.nombreCategoria}
+                    </p>
+                </Card.Header>
+             <Card.Body>
+             <Card.Title>
+               <h1>
+                 {not.titulo}
+               </h1>
+             </Card.Title>
+             <Card.Subtitle className="mb-2 text-muted">
+               <h3>
+                 {not.descripcion}
+               </h3>
+             </Card.Subtitle>
+             <div className="row">
+               <div className="col-sm-12 col-md-8">
+                 <p>
+                   {not.descripNoticia}
+                 </p>
+               </div>
+               <div className="col-sm-12 col-md-4">
+               <div className="row">
+                 <div className="col-12 mb-2">
+                   <Figure>
+                     <Image
+                       className="img-fluid "
+                       src={not.foto}
+                     />
+                   </Figure>
+                 </div>
+                 <div className="col-12 border mb-2">
+                   <h5>
+                     {not.pieDeFoto}
+                   </h5>
+                 </div>
+               </div>
+               </div>
+             </div>
+           </Card.Body>
+           <Card.Footer className="text-muted">
+             <h5>{not.autor}</h5>
+           </Card.Footer>
       </Card>
+              )
+            }
+          })}
       <div className="d-flex justify-content-center mb-5">
         <Link className="btn btn-info text-light w-75" to={`/menu-noticias/${tok}`}>
           Volver a Noticias
