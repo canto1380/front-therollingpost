a6f95a3db3cb4780b9910097eb84953898815aa4
Pablo Julian Alonso Olivera <pepoalonso1@gmail.com>
2021-06-24 23:06:15 -0300

    Limitacion de cards por categoria destacada en inicio

--- src/components/CategoriaDestacada.js
+++ src/components/CategoriaDestacada.js
@@ -6,6 +6,8 @@ import { faComment} from "@fortawesome/free-solid-svg-icons";
 
 const CategoriaDestacada = (props) => {
     const { noticias, cat, comentario } = props
+    
+    var notXCat = noticias.filter((not)=>(not.categoria?.nombreCategoria === props.cat.nombreCategoria)).slice(0,4);
 
     return (
         <section className="my-5 w-100" >
@@ -15,35 +17,30 @@ const CategoriaDestacada = (props) => {
             </div>
             <hr />
             <Row>
-                {noticias.map((not) => {
-                        if (not.categoria?.nombreCategoria === props.cat.nombreCategoria) {
-                            let coment = comentario.filter((c) => c.idNoticia?._id === not._id);
-                            let comentLength = coment.length; 
-                            return (
-                                <Col xs={12} md={6} lg={3} key={not._id} >
-                                    <Link to={`/noti/${not.categoria.nombreCategoria}/${not._id}`}
-                                     className="text-dark text-decoration-none" >
-                                        <div className="card tarjetaNoticia cardTall mt-3 border-0">
-                                            <div className="imgSpace d-flex align-content-start">
-                                              <img src={not.foto} alt=""/>
-                                            </div>
-                                            <div className="card-body tarjetaNoticia-body">
-                                                <h5 className="card-title titleShort">{not.titulo}</h5>
-                                                <p className="card-text textShort">{not.descripcion}</p>
-                                            </div>
-                                            <div className="d-flex justify-content-between align-items-center mb-3 mx-3">
-                                                <p className="my-0 text-muted">{not.hora} hs | {not.fecha}</p>
-                                                <p className="my-0 text-muted">{comentLength}<FontAwesomeIcon icon={faComment} size="1x" className="ms-1"></FontAwesomeIcon></p>
-                                            </div>
-                                        </div>  
-                                    </Link>
-                                </Col>
+                {notXCat.map((not) => {
+                     let coment = comentario.filter((c) => c.idNoticia?._id === not._id);
+                     let comentLength = coment.length; 
+                     return (
+                         <Col xs={12} md={6} lg={3} key={not._id} >
+                            <Link to={`/noti/${not.categoria.nombreCategoria}/${not._id}`}
+                             className="text-dark text-decoration-none" >
+                                 <div className="card tarjetaNoticia cardTall mt-3 border-0">
+                                    <div className="imgSpace d-flex align-content-start">
+                                      <img src={not.foto} alt=""/>
+                                    </div>
+                                    <div className="card-body tarjetaNoticia-body">
+                                       <h5 className="card-title titleShort">{not.titulo}</h5>
+                                       <p className="card-text textShort">{not.descripcion}</p>
+                                    </div>
+                                    <div className="d-flex justify-content-between align-items-center mb-3 mx-3">
+                                       <p className="my-0 text-muted">{not.hora} hs | {not.fecha}</p>
+                                       <p className="my-0 text-muted">{comentLength}<FontAwesomeIcon icon={faComment} size="1x" className="ms-1"></FontAwesomeIcon></p>
+                                    </div>
+                                 </div>  
+                            </Link>
+                         </Col>
                             )
-                        }
-                        return null
-                    }
-                    )
-                }
+                    })}
             </Row>
         </section>
     );
